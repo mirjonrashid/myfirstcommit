5d5a08a9e5465c6ab18e2a0ba293184d20259213
Mirjon Rashid <162153982+mirjonrashid@users.noreply.github.com>
2024-05-08 12:24:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,32 +45,7 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 
-  // // When guess is too high
-  // else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = ' 🔺 Too high!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = ' 🤯 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //     document.querySelector('body').style.backgroundColor = 'red';
-  //     document.querySelector('.number').textContent = secretNumber;
-  //   }
-
-  //   // When guess is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '🔻 Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = ' 🤯 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //     document.querySelector('body').style.backgroundColor = 'red';
-  //     document.querySelector('.number').textContent = secretNumber;
-  //   }
-  // }
+ 
 });
 
 document.querySelector('.again').addEventListener('click', function () {
